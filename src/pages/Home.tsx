4a9d603e21962d63eea1f71dbecf508cac1b3f52
brahman98 <bergen.bargen@gmail.com>
2026-05-25 18:47:50 +0300

    init project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <section className="hero">
+        <p>ЙОГ С КАВКАЗА</p>
+        <h1>viktor.yoga</h1>
+        <p>спортсмен • музыкант • программист</p>
+      </section>
+    </div>
+  );
+}
